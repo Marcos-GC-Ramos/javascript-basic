c49e963bd75bc595ec839e1c302bd064b45452ed
Marcos Ramos <74572110+Marcos-GC-Ramos@users.noreply.github.com>
2023-03-07 21:48:51 -0300

    Delete Aulas JS directory

--- Aulas JS/TiposdeFalsoeVerdadeiros.js
+++
@@ -1,9 +0,0 @@
-const primeiroNumero = 5;
-const segundoNumero = 5;
-
-console.log(primeiroNumero === segundoNumero); // True
-
-const texto1 = "Alura";
-const texto2 = "alura";
-
-console.log(texto1 === texto2); // False
